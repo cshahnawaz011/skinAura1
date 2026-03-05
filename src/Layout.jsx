84bea688c6ca8929fb012d60b2ebda8a60e5ac20
shahnawaz chaudhary <cshahnawaz011@gmail.com>
2026-03-05 07:32:10 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -180,7 +180,7 @@ export default function Layout({ children, currentPageName }) {
       </nav>
 
       {/* Main Content */}
-      <main className="lg:ml-64 min-h-screen pt-[72px] lg:pt-0 pb-24 lg:pb-0">
+      <main className="lg:ml-64 min-h-screen pt-[72px] lg:pt-0 pb-[calc(72px+env(safe-area-inset-bottom,0px))] lg:pb-0">
         <AnimatePresence mode="wait">
           <motion.div
             key={currentPageName}
