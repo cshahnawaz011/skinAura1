a463d734c0524676d37001eb3a69873924975709
shahnawaz chaudhary <cshahnawaz011@gmail.com>
2026-03-06 17:58:58 +0000

    File changes

--- src/components/home/ProactiveHealthInsights.jsx
+++ src/components/home/ProactiveHealthInsights.jsx
@@ -184,7 +184,7 @@ Examples of good insights (use their real data, not these examples):
     setInsights(result.insights || []);
     setHealthScore({ score: result.overall_health_score, summary: result.health_summary });
     recordAIUsage('health_insights');
-    setCooldownLeft(5 * 60);
+    setCooldownLeft(3 * 60);
     setLoading(false);
   };
 
